17f777b6a9ad924a9158d5b8edd32daef6be11c4
cw <fromnowwon@gmail.com>
2024-07-07 22:44:29 +0900

    fix: 무비차트 페이지 빈 배열 경우 고려

--- src/components/Movies/Chart/index.jsx
+++ src/components/Movies/Chart/index.jsx
@@ -3,17 +3,20 @@ import Card from "./Card";
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default async function Chart() {
+  let movies = [];
+
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`
   );
   const data = await response.json();
-  let movies = data.results;
+  movies = data.results || [];
+
   // 인기도 내림차순
   movies = movies.sort((a, b) => b.popularity - a.popularity);
 
   return (
     <div className="sm:grid sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5 gap-9 mt-6">
-      {movies &&
+      {movies.length > 0 &&
         movies.map((movie, index) => (
           <Card key={movie.id} movie={movie} index={index} />
         ))}
