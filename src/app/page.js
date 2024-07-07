141fa828fd83973adf21ee35b75694705af07f2e
cw <fromnowwon@gmail.com>
2024-07-07 22:06:40 +0900

    style: main 페이지 패딩 추가

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Trending from "@components/Main/Trending";
 
 export default function Home() {
   return (
-    <main>
+    <main className="pb-10">
       <div className="container mx-auto">
         <TopRated />
         <Trending />
